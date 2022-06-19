9cb63f287f5995b3ba590ddd0f731c0010354254
chaosError <chaosError@users.noreply.github.com>
2022-06-19 22:53:20 +0800

    Update xapi_json_scheme.js

--- xapi_json_scheme.js
+++ xapi_json_scheme.js
@@ -1,6 +1,6 @@
 /* xapi记录如下数据：
     `object.id`属性记录了用户当前所选定课程id，
-    `verb.id`属性记录了用户对于当前课程推荐的每一门课是否跟随链接，如"recommend1"代表用户点击了正向推荐列表中的第一门课程，"recommend-3"代表用户点击了逆向推荐列表中的第三门课程
+    `verb.id`属性记录了用户对于当前课程推荐的每一门课是否跟随链接，如"recommend1"代表用户点击了相似推荐列表中的第一门课程，"recommend-3"代表用户点击了不相似推荐列表中的第三门课程
 */
 var VERB_LIST = ["recommend1", "recommend2", "recommend3", "recommend4", "recommend5", 
                  "recommend-1", "recommend-2", "recommend-3", "recommend-4", "recommend-5"];
